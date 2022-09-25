33499d827ed93fdfe094fa70c9f273a9a99154ec
Anatoly Borisov <drfoxg@gmail.com>
2022-09-25 20:50:57 +0300

    Top views

--- js/mainData.js
+++ js/mainData.js
@@ -15,7 +15,7 @@ const mainData = () => {
         wrapper.innerHTML = '';
         array.forEach(item => {
 
-            wrapper.insertAdjacentHTML('afterbegin', `
+            wrapper.insertAdjacentHTML('beforeend', `
                 <div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
                     <div class="ep">${item.rating} / 10</div>
                     <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
