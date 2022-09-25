91d0af80fa83977298993542e92260b6b0bc4caa
Anatoly Borisov <drfoxg@gmail.com>
2022-09-25 11:59:33 +0300

    Top views

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,15 +1,24 @@
 
-const bgElements = () => {
+const bgElements = (classBg, node = document) => {
 
-    const elements = document.querySelectorAll('.set-bg');
+    const elements = node.querySelectorAll(classBg);
 
+    elements.forEach((imageBlock, indexImageBlock, array) => {
+
+        const src = imageBlock.dataset.setbg;
+        imageBlock.style.backgroundImage = `url(${src})`;
+
+    });
+
+    /* так не модно перебирать массив
     for (let i = 0; i < elements.length; i++) {
 
         const src = elements[i].dataset.setbg;
         elements[i].style.backgroundImage = `url(${src})`;
 
     }
+    */
 
 };
 
-bgElements();
+bgElements('.set-bg');
--- js/mainData.js
+++ js/mainData.js
@@ -1,13 +1,56 @@
 
 const mainData = () => {
 
+    const renderAnimeList = (array, ganres) => {
+
+        console.log('ganres: ', ganres);
+        console.log('array: ', array);
+
+    }
+
+    const renderTopAnime = (array) => {
+
+        const wrapper = document.querySelector('.filter__gallery');
+
+        wrapper.innerHTML = '';
+        array.forEach(item => {
+
+            wrapper.insertAdjacentHTML('afterbegin', `
+                <div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
+                    <div class="ep">${item.rating} / 10</div>
+                    <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+                    <h5><a href="/anime-details.html">${item.title}</a></h5>
+                </div>              
+            `);
+
+        });
+
+        bgElements('.set-bg', wrapper);
+
+    }
+
     fetch('https://anime-intensity-default-rtdb.europe-west1.firebasedatabase.app/anime.json')
         .then((responce) => {
             return responce.json()
         })
         .then((anime) => {
-            console.log('data: ', anime);
+
+            console.log('anime: ', anime);
+
+            const ganres = new Set();
+
+            // вывод первых 5 самых просматриваемых аниме на основе значения views
+            renderTopAnime(anime.sort((a, b) => b.views - a.views).slice(0, 5));
+
+            anime.forEach((items) => {
+                ganres.add(items.ganre);
+            });
+
+            renderAnimeList(anime, ganres);
+
         });
+
+
 
 };
 
